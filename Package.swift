fbf047f2640a12b0c9f636772d7dc1c57ff221fc
Mwadime <brian.mwadime@gmail.com>
2023-10-30 21:58:09 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,10 +1,11 @@
-// swift-tools-version:5.0
+// swift-tools-version: 5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "ImageViewer",
+    platforms: [.iOS(.v14)],
     products: [
         .library(
             name: "ImageViewer",
@@ -13,6 +14,10 @@ let package = Package(
     targets: [
         .target(
             name: "ImageViewer",
-            path: "ImageViewer")
+            dependencies: []
+        ),
+        .testTarget(
+            name: "ImageViewerTests",
+            dependencies: ["ImageViewer"]),
     ]
 )
